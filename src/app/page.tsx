5a19e74a119101f4d4ed0450a1686ea6c129c120
Aaron Lambley <aaron.lambley@gmail.com>
2024-02-07 11:15:43 +0000

    add tensorflow scripts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Script
+        src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs"
+        strategy="beforeInteractive"
+      ></Script>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/@tensorflow-models/qna"
+        strategy="beforeInteractive"
+      ></Script>
       <h1 className="text-5xl font-bold">Hello World</h1>
     </main>
   );
