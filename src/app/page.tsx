b72d551ed5e13d41b6534da0e17b277b3ead8960
Aaron Lambley <aaron.lambley@gmail.com>
2024-03-06 09:20:14 +0000

    update lint configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ChatWindow from "@/components/ChatWindow";
 import QnaModel from "@/lib/tensorflowInit";
 import { Answers } from "@/types";
