5ac00350980a995b10c4bb485a62a849d1955557
Aaron Lambley <aaron.lambley@gmail.com>
2024-03-20 09:38:16 +0000

    update home page rendering tests

--- src/app/test/page.test.tsx
+++ src/app/test/page.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import Home from "@/app/page";
 import "@testing-library/jest-dom/extend-expect";
 import mockQnaModel from "@/__mocks__/mockQnaModel";
@@ -16,9 +16,26 @@ describe("Home", () => {
       render(<Home />);
     });
 
-    it("renders loading message", () => {
+    it("renders initial loading message", () => {
       const { getByText } = render(<Home />);
       expect(getByText("Loading TensorFlow.js scripts...")).toBeInTheDocument();
+    });
+
+    it("renders loaded message", async () => {
+      const { getByText } = render(<Home />);
+      waitFor(
+        () => {
+          expect(
+            getByText("TensorFlow.js scripts have loaded!")
+          ).toBeInTheDocument();
+        },
+        { timeout: 3000 }
+      );
+    });
+
+    it("renders chat window", () => {
+      render(<Home />);
+      expect(document.querySelector("#chat-window")).toBeInTheDocument();
     });
   });
 });
